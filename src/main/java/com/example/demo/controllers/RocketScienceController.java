1c0ce778d27689ad5a1d35cef4516ae55dd57cf7
Alexander <alex030t@stud.kea.dk>
2021-03-10 15:50:17 +0100

    Added controller and getmapping

--- src/main/java/com/example/demo/controllers/RocketScienceController.java
+++ src/main/java/com/example/demo/controllers/RocketScienceController.java
@@ -1,4 +1,15 @@
 package com.example.demo.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class RocketScienceController {
+
+
+    @GetMapping(value="/")
+    public String mainPage(){
+
+        return "index.html";
+    }
 }
